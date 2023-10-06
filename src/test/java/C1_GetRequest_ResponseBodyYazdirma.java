ecc642974c36afc440c9ed998beb3996aa7fcfb7
ToKyOzY <zynpsarkaya@gmail.com>
2023-10-06 13:26:51 +0300

    api

--- src/test/java/C1_GetRequest_ResponseBodyYazdirma.java
+++ src/test/java/C1_GetRequest_ResponseBodyYazdirma.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class C1_GetRequest_ResponseBodyYazdirma {
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C1_GetRequest_ResponseBodyYazdirma {
+
+    @Test
+    public void get01(){
+
+        /*
+        https://restful-booker.herokuapp.com/booking/10 url’ine bir GET request gonderdigimizde
+        donen Response’u yazdirin.
+         */
+
+        //1- Request body ve end-point hazırlama
+
+        String url="https://restful-booker.herokuapp.com/booking/10";
+
+        //2- Expected data hazırlama
+
+
+        //3- Request gonderip, donen response'i kaydetme
+
+        Response response=given().when().get(url);
+        response.prettyPrint();
+
+        //4- Assertion
+
+
+    }
+
 }
