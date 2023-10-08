ca758900a161d6c96d4c3488268da63e18a544a7
ToKyOzY <zynpsarkaya@gmail.com>
2023-10-08 15:44:33 +0300

    api

--- src/test/java/C06_Put_ResponseBilgileriAssertion.java
+++ src/test/java/C06_Put_ResponseBilgileriAssertion.java
@@ -1,4 +1,8 @@
+import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class C06_Put_ResponseBilgileriAssertion {
 
@@ -22,6 +26,29 @@ public class C06_Put_ResponseBilgileriAssertion {
          */
 
 
+        // 1- End-point ve request body olustur
+
+        String url= "https://jsonplaceholder.typicode.com/posts/70";
+
+        JSONObject requestBody=new JSONObject();
+        requestBody.put("title","Ahmet");
+        requestBody.put("body","Merhaba");
+        requestBody.put("userId",10);
+        requestBody.put("id",70);
+
+        // 2- expected body olustur
+
+        // 3- Request gonderip, doen response'i kaydet
+        Response response=given().when().body(requestBody.toString()).
+                            put(url);
+        //response.prettyPrint();
+
+        // 4- Assertion
+        response.then().assertThat()
+                .statusCode(200)
+                .contentType("application/json; charset=utf-8")
+                .header("Server","cloudflare")
+                .statusLine("HTTP/1.1 200 OK");
     }
 
 }
---
+++ src/test/java/C08_Post_ResponseBodyTesti.java
@@ -0,0 +1,59 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C08_Post_ResponseBodyTesti {
+    @Test
+    public void test01(){
+        /*
+        https://jsonplaceholder.typicode.com/posts url’ine asagidaki body ile bir POST request
+        gonderdigimizde
+        {
+        "title":"API",
+        "body":"API ogrenmek ne guzel",
+        "userId":10,
+        }
+        donen Response’un,
+        status code’unun 201,
+        ve content type’inin application/json
+        ve Response Body'sindeki,
+        "title"'in "API" oldugunu
+        "userId" degerinin 100'den kucuk oldugunu
+        "body" nin "API" kelimesi icerdigini
+        test edin.
+         */
+
+        // 1- End-point ve request body olustur
+
+
+        String url="https://jsonplaceholder.typicode.com/posts";
+
+        JSONObject requestBody=new JSONObject();
+        requestBody.put("title","API");
+        requestBody.put("body","API ogrenmek ne guzel");
+        requestBody.put("userId",10);
+
+        // 2- expected body olustur
+
+        // 3- Request gonderip, doen response'i kaydet
+
+        Response response=given().contentType(ContentType.JSON)
+                .when().body(requestBody.toString())
+                .post(url);
+
+        //4- Assertion
+        response.then()
+                .assertThat()
+                .statusCode(201)
+                .contentType(ContentType.JSON)
+                .body("title", Matchers.equalTo("API"))
+                .body("body", Matchers.containsString("API"))
+                .body("userId", Matchers.lessThan(100));
+
+
+    }
+}
